10790faf20b0e83ec9a9f4ce80b0b963ceae8212
oskarhane <oskarhane@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-10 10:03:56 +0000

    Added tag 4.2.2
    git-svn-id: http://plugins.svn.wordpress.org/two-factor-auth/tags/4.2.2@738636 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tfa_v4.2.2.js
@@ -0,0 +1,82 @@
+jQuery(document).ready(function() {
+    var user_field = document.getElementById('user_login');
+	var pw_field = document.getElementById('user_pass');
+	var submit_btn = document.getElementById('wp-submit');
+	var remember_me = document.getElementById('rememberme');
+	submit_btn.disabled = true;
+	
+	var otp_btn = document.createElement('button');
+	otp_btn.id = 'otp-button';
+	otp_btn.className = 'button button-large button-primary';
+	otp_btn.onclick = function(){ return tfaChangeToInput(); };
+	otp_btn.style.styleFloat = 'none';
+	otp_btn.style.cssFloat = 'none';
+	
+	var btn_text = document.createTextNode(tfaSettings.click_to_enter_otp);
+	otp_btn.appendChild(btn_text);
+	otp_btn.style.width = '100%';
+	
+	var p = document.createElement('p');
+	p.id = 'tfa_holder';
+	p.style.marginBottom = '15px';
+	
+	p.appendChild(otp_btn);
+	tfaAddToForm(p);
+	
+	function tfaChangeToInput()
+	{
+		//Check so a username is entered.
+		if(jQuery('#user_login').val().length < 1)
+		{
+			alert(tfaSettings.enter_username_first);
+			return false;
+		}
+		
+		jQuery.post(
+			tfaSettings.ajaxurl,
+			{
+				action : 'tfa-init-otp',
+				user : jQuery('#user_login').val()
+			},
+			function( response ) {
+			}
+		);
+		
+		var p = document.getElementById('tfa_holder');
+		var lbl = document.createElement('label');
+		lbl.for = 'two_factor_auth';
+		var lbl_text = document.createTextNode(tfaSettings.otp);
+		lbl.appendChild(lbl_text);
+		
+		var br = document.createElement('br');
+		lbl.appendChild(br);
+		
+		var small_lbl = document.createElement('span');
+		small_lbl.style.fontSize = '0.8em';
+		var small_lbl_text = document.createTextNode(tfaSettings.otp_login_help);
+		small_lbl.appendChild(small_lbl_text);
+		lbl.appendChild(small_lbl);
+		
+		var tfa_field = document.createElement('input');
+		tfa_field.type = 'text';
+		tfa_field.id = 'two_factor_auth';
+		tfa_field.name = 'two_factor_code';
+		lbl.appendChild(tfa_field);
+		
+		//Remove button
+		p.removeChild(document.getElementById('otp-button'));
+		
+		//Add text and input field
+		p.appendChild(lbl);
+		tfa_field.focus();
+		
+		//Enable regular submit button
+		document.getElementById('wp-submit').disabled = false;
+	}
+	
+	function tfaAddToForm(p)
+	{
+		document.getElementById('loginform').insertBefore(p, document.getElementById('rememberme').parentNode.parentNode);
+	}
+
+});
