89cd8357de2f4b6c2c556505287438241a50f7c8
oskarhane <oskarhane@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-10 13:00:06 +0000

    Modified files
    git-svn-id: http://plugins.svn.wordpress.org/two-factor-auth/trunk@890565 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- two-factor-login.php
+++ two-factor-login.php
@@ -5,7 +5,7 @@ Plugin URI: http://oskarhane.com/plugin-two-factor-auth-for-wordpress
 Description: Secure your WordPress login with two factor auth. Users will be prompted with a page to enter a One Time Password when they login.
 Author: Oskar Hane
 Author URI: http://oskarhane.com
-Version: 4.3.3
+Version: 4.3.4
 License: GPLv2 or later
 */
 //error_reporting(E_ALL);
@@ -269,7 +269,7 @@ function tfaAddJSToLogin()
 	if(isset($_GET['action']) && $_GET['action'] != 'logout' && $_GET['action'] != 'login')
 		return;
 	
-	wp_enqueue_script( 'tfa-ajax-request', plugin_dir_url( __FILE__ ) . 'tfa_v4.3.js', array( 'jquery' ) );
+	wp_enqueue_script( 'tfa-ajax-request', plugin_dir_url( __FILE__ ) . 'tfa_v4.3.4.js', array( 'jquery' ) );
 	wp_localize_script( 'tfa-ajax-request', 'tfaSettings', array(
 		'ajaxurl' => admin_url('admin-ajax.php'),
 		'click_to_enter_otp' => __("Click to enter One Time Password", TFA_TEXT_DOMAIN),
