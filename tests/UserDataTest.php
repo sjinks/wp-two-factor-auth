03622d21168ce0298a358acd7eae86170f1801d7
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2018-04-02 04:04:25 +0300

    More tests

--- tests/UserDataTest.php
+++ tests/UserDataTest.php
@@ -8,7 +8,7 @@ class UserDataTest extends TestCase
 	public function test2FAEnabled()
 	{
 		$user_id = 1;
-		$data    = new UserData(1);
+		$data    = new UserData($user_id);
 
 		$this->assertFalse($data->is2FAEnabled());
 		$this->assertFalse($data->is2FAForcefullyEnabled());
@@ -30,5 +30,51 @@ class UserDataTest extends TestCase
 		$data->setDeliveryMethod('');
 		$this->assertTrue($data->is2FAEnabled());
 		$this->assertEquals('email', $data->getDeliveryMethod());
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testSetDeliveryMethodException()
+	{
+		$data = new UserData(1);
+		$data->setDeliveryMethod('invalid');
+	}
+
+	/**
+	 * @expectedException \InvalidArgumentException
+	 */
+	public function testSetHMACMethodException()
+	{
+		$data = new UserData(1);
+		$data->setHMAC('invalid');
+	}
+
+	public function testResetPrivateKey()
+	{
+		$data = new UserData(1);
+		$data->setDeliveryMethod('email');
+
+		$pk1 = $data->getPrivateKey();
+		$this->assertEquals('hotp', $data->getHMAC());
+		$this->assertEmpty($data->getPanicCodes());
+		$this->assertEmpty($data->getUsedCodes());
+
+		$data->resetPrivateKey();
+		$pk2 = $data->getPrivateKey();
+
+		$this->assertNotEquals($pk1, $pk2);
+	}
+
+	public function testGeneratePanicCodes()
+	{
+		$data  = new UserData(1);
+		$codes = $data->generatePanicCodes();
+		$this->assertNotEmpty($codes);
+		$this->assertCount(UserData::$panicCodes, $codes);
+
+		foreach ($codes as $code) {
+			$this->assertEquals(UserData::$panicCodeLength, strlen($code));
+		}
 	}
 }
