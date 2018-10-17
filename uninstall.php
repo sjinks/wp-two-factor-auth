c28f2c26c18c9176e74d542b2a61bf07022ccc53
Volodymyr Kolesnykov <sjinks@users.noreply.github.com>
2018-10-17 16:33:40 +0300

    Don't mess with the database, use WP API

--- uninstall.php
+++ uninstall.php
@@ -1,6 +1,5 @@
 <?php
 defined('WP_UNINSTALL_PLUGIN') || die();
 
-global $wpdb;
 delete_option('tfa');
-$wpdb->delete($wpdb->usermeta, ['meta_key' => 'tfa']);
+delete_metadata('user', 0, 'tfa', '', true);
