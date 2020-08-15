d722f46f95680451e088036fd3320d80e4a4595e
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2020-08-15 17:45:00 +0300

    5.1.9

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  Plugin URI: https://github.com/sjinks/wp-two-factor-auth
  Description: HOTP/TOTP based two factor authentication for WordPress
  Author: Volodymyr Kolesnykov
- Version: 5.1.8
+ Version: 5.1.9
  License: MIT
  */
 
